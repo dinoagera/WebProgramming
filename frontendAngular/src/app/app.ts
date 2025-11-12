d15e0a1f0eb677af4bbfe28ce8aeb72769342d0e
Lona <holyloran@gmail.com>
2025-11-12 15:53:16 +0300

    Нафлудил товрами, сделал второй навбар(catalog)

--- frontendAngular/src/app/app.ts
+++ frontendAngular/src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CatalogProduct } from './common-ui/catalog-product/catalog-product';
 import { Navbar } from './common-ui/navbar/navbar';
+import { SecondNavbar } from './common-ui/second-navbar/second-navbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CatalogProduct, Navbar],
+  imports: [RouterOutlet, CatalogProduct, Navbar, SecondNavbar],
 
   templateUrl: './app.html', //отсюда берется html
   styleUrl: './app.scss', //отсюда берутся стили
---
+++ frontendAngular/src/app/common-ui/second-navbar/second-navbar.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-second-navbar',
+  imports: [],
+  templateUrl: './second-navbar.html',
+  styleUrl: './second-navbar.scss',
+})
+export class SecondNavbar {
+
+}
