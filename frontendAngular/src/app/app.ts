e1c89733b77ba263e6b849724ddb656604f132bd
Lona <holyloran@gmail.com>
2025-11-12 15:38:43 +0300

    начало объединения ангуляр и каталога

--- frontendAngular/src/app/app.ts
+++ frontendAngular/src/app/app.ts
@@ -1,11 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CatalogProduct } from './common-ui/catalog-product/catalog-product';
+import { Navbar } from './common-ui/navbar/navbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.scss'
+  imports: [RouterOutlet, CatalogProduct, Navbar],
+
+  templateUrl: './app.html', //отсюда берется html
+  styleUrl: './app.scss', //отсюда берутся стили
 })
 export class App {
   protected readonly title = signal('frontendAngular');
---
+++ frontendAngular/src/app/common-ui/catalog-product/catalog-product.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-catalog-product',
+  imports: [],
+  templateUrl: './catalog-product.html',
+  styleUrl: './catalog-product.scss',
+})
+export class CatalogProduct {
+
+}
