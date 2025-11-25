235ea717baaa2f8184a1f6b933c7cfda8ea2d16e
Maaric <holyloran@gmail.com>
2025-11-25 23:09:51 +0300

    Стыдный вариант main

--- frontendAngular/src/app/pages/main-page/main-page.ts
+++ frontendAngular/src/app/pages/main-page/main-page.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Navbar } from '../../common-ui/navbar/navbar';
+import { Footer } from '../../common-ui/footer/footer';
 
 @Component({
   selector: 'app-main-page',
-  imports: [Navbar],
+  imports: [Navbar,Footer],
   templateUrl: './main-page.html',
   styleUrl: './main-page.scss',
 })
