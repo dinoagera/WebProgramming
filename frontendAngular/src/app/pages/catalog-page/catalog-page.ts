349e8be9b3faf9ace63f27dd61dfd683880bfec9
Maaric <holyloran@gmail.com>
2025-11-25 22:56:22 +0300

    searc сверстан

--- frontendAngular/src/app/pages/catalog-page/catalog-page.ts
+++ frontendAngular/src/app/pages/catalog-page/catalog-page.ts
@@ -3,13 +3,14 @@ import { ProductCatalog } from '../../data/services/product-catalog';
 import { Product } from '../../data/interfaces/profile.interfaces';
 import { Navbar } from '../../common-ui/navbar/navbar';
 import { CatalogProduct } from '../../common-ui/catalog-product/catalog-product';
-
+import { SecondNavbar } from '../../common-ui/second-navbar/second-navbar';
+import { Footer } from '../../common-ui/footer/footer';
 
 
 
 @Component({
   selector: 'app-catalog-page',
-  imports: [Navbar, CatalogProduct],
+  imports: [Navbar, CatalogProduct, SecondNavbar, Footer],
   templateUrl: './catalog-page.html',
   styleUrl: './catalog-page.scss',
 })
